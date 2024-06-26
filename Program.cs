77f224d614582fd84b6f362c367b47b01cc6fbe4
Lasse <LauwerysLasse@gmail.com>
2024-06-26 02:45:02 +0200

    Scanning works, I need to properly reverse engineer the file format still to get proper file names and more metadata but for now, just scanning for audio data works.

--- Program.cs
+++ Program.cs
@@ -20,77 +20,135 @@ namespace VXVSUnpacker
             byte[] magic = new byte[8];
             file.Read(magic, 0, 4);
 
-            if (Encoding.UTF8.GetString(magic) == "Vxvs")
+            Console.WriteLine(Encoding.UTF8.GetString(magic));
+            if (Encoding.UTF8.GetString(magic, 0, 4) == "Vxvs")
             {
                 file.Read(magic, 0, 4);
+                int ten = BitConverter.ToInt32(magic, 0);
 
-                StringBuilder stringBuilder = new StringBuilder();
-                int versionChunk;
+                file.Read(magic, 0, 8);
 
-                while ((versionChunk = file.ReadByte()) != 0) stringBuilder.Append(versionChunk);
-
-                if (stringBuilder.ToString() != "0.0.3")
+                if (Encoding.UTF8.GetString(magic) != "0.0.3")
                 {
                     Console.WriteLine("Unteseted version!");
                 }
 
                 byte[] stuff = new byte[8];
-                file.Read(stuff, 0, 8);
+                file.Read(stuff, 0, 4);
 
-                byte[] thingLength = new byte[2];
-                file.Read(thingLength, 0, 2);
+                int dataStart = BitConverter.ToInt32(stuff, 0);
 
-                short length = BitConverter.ToInt16(thingLength, 0);
+                file.Read(stuff, 0, 4);
 
-                byte[] data = new byte[length];
-                file.Read(data, 0, length);
+                int dataEnd = BitConverter.ToInt32(stuff, 0);
 
-                Console.WriteLine(Encoding.UTF8.GetString(data));
+                int dataLength = dataEnd - dataStart;
 
-                file.Read(magic, 0, 4);
-                file.Read(magic, 0, 4);
+                byte[] dataBuffer = new byte[dataLength];
 
-                int segmentCount = BitConverter.ToInt32(magic, 0);
-
-                Console.WriteLine(segmentCount + "segments");
-
-                byte[] another = new byte[28];
-
-                for (int i = 0; i < segmentCount; ++i)
+                if (crack)
                 {
-                    file.Read(another, 0, 28);
-                }
+                    // Skip to the data and scan it for data.
 
-                file.Read(magic, 0, 4);
-                Console.WriteLine(Encoding.UTF8.GetString(magic));
+                    int buffer;
 
-                if (Encoding.UTF8.GetString(magic) == "Cfgs")
-                {
-                    //byte[] thing = new byte[48];
-                    //file.Read(thing, 0, 48);
+                    int bufferPosition = 0;
 
-                    file.Read(stuff, 0, 4);
-                    file.Read(stuff, 0, 8);
+                    int magicFound = 0;
 
-                    byte[] nameBuffer = new byte[32];
-                    file.Read(nameBuffer, 0, 32);
-                    Console.WriteLine("Found thing: " + nameBuffer);
+                    int fileFound = 0;
 
-                    file.Read(magic, 0, 4);
-                    file.Read(magic, 0, 4);
+                    int headersFound = 0;
 
-                    string conf = Encoding.UTF8.GetString(magic);
-                    Console.WriteLine(conf);
-                    if (conf == "Conf")
+                    file.Seek(dataStart, SeekOrigin.Begin);
+
+                    FileStream output;
+
+                    while ((buffer = file.ReadByte()) != -1)
                     {
+                        byte nextByte = (byte)buffer;
+
+                        dataBuffer[bufferPosition++] = (byte)buffer;
+
+                        if (buffer == 'R' || (magicFound == 1 && buffer == 'I') || ((magicFound == 2 || magicFound == 3) && buffer == 'F')) magicFound++;
+                        else magicFound = 0;
+
+                        if (magicFound == 4)
+                        {
+                            headersFound++;
+                            magicFound = 0;
+
+                            if (headersFound > 1)
+                            {
+                                headersFound = 0;
+                                bufferPosition -= 4;
+                                file.Seek(-4, SeekOrigin.Current);
+                                output = File.OpenWrite("filefound" + fileFound++ + ".wav");
+
+                                output.Write(dataBuffer, 0, bufferPosition);
+                            }
+                        }
 
                     }
+
+                    output = File.OpenWrite("filefound" + fileFound++ + ".wav");
+                    output.Write(dataBuffer, 0, bufferPosition);
                 }
-            }
-            else
-            {
-                Console.WriteLine("Invalid file.");
-            }
+                else
+                {
+                    // Read the file as intended.
+                    byte[] data = new byte[4];
+                    file.Read(data, 0, 4);
+
+                    Console.WriteLine(Encoding.UTF8.GetString(data));
+
+                    file.Read(magic, 0, 4);
+                    file.Read(magic, 0, 4);
+
+                    int segmentCount = BitConverter.ToInt32(magic, 0);
+
+                    Console.WriteLine(segmentCount + "segments");
+
+                    byte[] another = new byte[28];
+
+                    for (int i = 0; i < segmentCount; ++i)
+                    {
+                        file.Read(another, 0, 28);
+                    }
+
+                    file.Read(magic, 0, 4);
+                    Console.WriteLine(Encoding.UTF8.GetString(magic));
+
+                    if (Encoding.UTF8.GetString(magic) == "Cfgs")
+                    {
+                        //byte[] thing = new byte[48];
+                        //file.Read(thing, 0, 48);
+
+                        file.Read(stuff, 0, 4);
+                        file.Read(stuff, 0, 8);
+
+                        byte[] nameBuffer = new byte[32];
+                        file.Read(nameBuffer, 0, 32);
+                        Console.WriteLine("Found thing: " + nameBuffer);
+
+                        file.Read(magic, 0, 4);
+                        file.Read(magic, 0, 4);
+
+                        string conf = Encoding.UTF8.GetString(magic);
+                        Console.WriteLine(conf);
+                        if (conf == "Conf")
+                        {
+
+                        }
+                    }
+                }
+
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid file.");
+                }
             Console.ReadLine();
         }
     }
